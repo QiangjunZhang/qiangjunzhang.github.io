408524a362792aa0083d0122f326a5825bc3c36a
qiangjun <zhangqj616@gmail.com>
2020-12-05 21:54:10 +0000

    add Whack a mole

---
+++ assets/js/whac_a_mole.js
@@ -0,0 +1,57 @@
+const square = document.querySelectorAll('.cell')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#user-score')
+const gameTime = 60
+const moleFreq = 500
+const numOfMoles = 9
+let result = 0
+let currentTime = timeLeft.textContent
+
+
+function randomSquare() {
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random()*numOfMoles)]
+  randomPosition.classList.add('mole')
+  hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+  id.addEventListener('mouseup', () => {
+    if (id.id === hitPosition){
+      result = result + 1
+      score.textContent = result
+    }
+  })
+})
+
+function moveMole() {
+  let timerID = null
+  timerID = setInterval(randomSquare, moleFreq)
+}
+
+function stopMole() {
+  clearInterval(timerID)
+}
+
+moveMole()
+
+function startGame() {
+  console.log(currentTime=== gameTime.toString());
+  if (currentTime === gameTime.toString()) {
+    let stop = setInterval(countDown, 1000)
+    function countDown() {
+      currentTime--
+      timeLeft.textContent = currentTime
+      if (currentTime === 0) {
+        clearInterval(stop)
+        timeLeft.textContent = gameTime
+        currentTime = timeLeft.textContent
+        score.textContent = 0
+        alert('GAME OVER Your final score is' + result)
+      }
+    }
+  }
+}
